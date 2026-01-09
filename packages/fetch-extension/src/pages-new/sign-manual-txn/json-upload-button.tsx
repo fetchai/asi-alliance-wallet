a2bb6ee63637e0dcf347b902a2c1e4d5c936d4bb
Abbas Ali <abbas.lokhandwala@fetch.ai>
2026-01-09 14:00:02 +0530

    feat: added upload button for raw txn and transaction details page ui update

---
+++ packages/fetch-extension/src/pages-new/sign-manual-txn/json-upload-button.tsx
@@ -0,0 +1,73 @@
+import React, { useRef } from "react";
+import { ButtonV2 } from "@components-v2/buttons/button";
+
+export const JsonUploadButton = ({
+  text,
+  onJsonLoaded,
+  onError,
+}: {
+  text: string;
+  onJsonLoaded: (data: any) => void;
+  onError: (error: string) => void;
+}) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    if (file.type !== "application/json") {
+      onError("Please upload a valid JSON file");
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      try {
+        const jsonStringified = reader.result;
+        onJsonLoaded(jsonStringified);
+      } catch (err) {
+        onError("Failed to read JSON file");
+      }
+    };
+
+    reader.readAsText(file);
+
+    // reset input so the same file can be uploaded again
+    e.target.value = "";
+  };
+
+  return (
+    <React.Fragment>
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="application/json"
+        onChange={handleFileChange}
+        hidden
+      />
+
+      <ButtonV2
+        text=""
+        styleProps={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "35px",
+          margin: "0px 0px 12px",
+          width: "fit-content",
+          fontSize: "14px",
+          fontWeight: 400,
+        }}
+        variant="dark"
+        onClick={handleButtonClick}
+      >
+        {text}
+      </ButtonV2>
+    </React.Fragment>
+  );
+};
