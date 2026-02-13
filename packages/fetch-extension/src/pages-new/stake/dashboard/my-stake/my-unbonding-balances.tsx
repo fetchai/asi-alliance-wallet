95c02741c8ef808d4cc032ab78fcdfb0074d38d9
Abbas Ali <abbas.lokhandwala@fetch.ai>
2026-02-13 18:28:37 +0530

    feat: add unbonding balances section in stake dashboard page

---
+++ packages/fetch-extension/src/pages-new/stake/dashboard/my-stake/my-unbonding-balances.tsx
@@ -0,0 +1,163 @@
+import { GlassCard } from "@components-v2/glass-card";
+import React, { useMemo } from "react";
+import styles from "./style.module.scss";
+import { observer } from "mobx-react-lite";
+import { Staking } from "@keplr-wallet/stores";
+import { useStore } from "../../../../stores";
+import { formatDistanceToNow } from "date-fns";
+
+export const MyUnbondingValidators = observer(() => {
+  const { chainStore, accountStore, queriesStore } = useStore();
+
+  const account = accountStore.getAccount(chainStore.current.chainId);
+  const queries = queriesStore.get(chainStore.current.chainId);
+
+  const queryUnbonding =
+    queries.cosmos.queryUnbondingDelegations.getQueryBech32Address(
+      account.bech32Address
+    );
+
+  const unbondings = queryUnbonding.unbondingBalances;
+
+  const bondedValidators = queries.cosmos.queryValidators.getQueryStatus(
+    Staking.BondStatus.Bonded
+  );
+  const unbondingValidators = queries.cosmos.queryValidators.getQueryStatus(
+    Staking.BondStatus.Unbonding
+  );
+  const unbondedValidators = queries.cosmos.queryValidators.getQueryStatus(
+    Staking.BondStatus.Unbonded
+  );
+
+  const validators = useMemo(
+    () =>
+      bondedValidators.validators
+        .concat(unbondingValidators.validators)
+        .concat(unbondedValidators.validators),
+    [
+      bondedValidators.validators,
+      unbondingValidators.validators,
+      unbondedValidators.validators,
+    ]
+  );
+
+  const validatorsMap = useMemo(() => {
+    const map = new Map<string, Staking.Validator>();
+    validators.forEach((v) => map.set(v.operator_address, v));
+    return map;
+  }, [validators]);
+
+  const unbondingBalancesCount = unbondings?.flatMap(
+    (item) => item.entries
+  ).length;
+
+  if (unbondings.length === 0 || unbondingBalancesCount === 0) {
+    return null;
+  }
+
+  return (
+    <div
+      className={styles["my-validators-container"]}
+      style={{
+        marginTop: "0px",
+      }}
+    >
+      <div style={{ display: "flex", gap: "6px", alignItems: "center" }}>
+        <div
+          style={{
+            color: "var(--font-dark)",
+            fontSize: "16px",
+            fontWeight: 400,
+          }}
+        >
+          Unbonding balances
+        </div>
+        <div className={styles["stake-count"]}>{unbondingBalancesCount}</div>
+      </div>
+
+      <div
+        className={styles["my-validators-container"]}
+        style={{
+          paddingBottom: "30px",
+        }}
+      >
+        {unbondings.map((ubd) => {
+          const val = validatorsMap.get(ubd.validatorAddress);
+          if (!val) return null;
+
+          const thumbnail =
+            bondedValidators.getValidatorThumbnail(val.operator_address) ||
+            unbondingValidators.getValidatorThumbnail(val.operator_address) ||
+            unbondedValidators.getValidatorThumbnail(val.operator_address);
+
+          return (
+            <React.Fragment key={ubd.validatorAddress}>
+              {ubd.entries.map((entry) => {
+                const remainingTime = formatDistanceToNow(
+                  new Date(entry.completionTime),
+                  { addSuffix: true }
+                );
+
+                return (
+                  <GlassCard
+                    key={`${ubd.validatorAddress}-${entry.completionTime}`}
+                  >
+                    <div className={styles["validator-div"]}>
+                      {thumbnail ? (
+                        <img src={thumbnail} alt="validator" />
+                      ) : (
+                        <div className={styles["validator-avatar"]}>
+                          {val.description.moniker?.[0]?.toUpperCase()}
+                        </div>
+                      )}
+
+                      <div className={styles["validator-details"]}>
+                        <div className={styles["validator-top"]}>
+                          <div className={styles["left-col"]}>
+                            <div style={{ fontWeight: 400 }}>
+                              {val.description.moniker}
+                            </div>
+
+                            <span
+                              className={styles["validator-currency"]}
+                              style={{ color: "var(--font-secondary)" }}
+                            >
+                              {entry.balance
+                                .maxDecimals(4)
+                                .trim(true)
+                                .shrink(true)
+                                .toString()}
+                            </span>
+                          </div>
+                        </div>
+
+                        <div
+                          style={{
+                            width: "100%",
+                            height: "1px",
+                            background: "var(--bg-grey-dark)",
+                          }}
+                        />
+
+                        <div className={styles["validator-bottom"]}>
+                          <span
+                            style={{
+                              fontSize: "12px",
+                              color: "var(--font-secondary)",
+                            }}
+                          >
+                            Unbonding • Completes {remainingTime}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </GlassCard>
+                );
+              })}
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
+});
